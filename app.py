fb1bb5ea88ac6107da99899a62bb6d6c910cc70f
Matthew Berthoud <mwberthoud@wm.edu>
2024-08-29 16:13:42 -0400

    MVP

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        print(request)
+        return redirect(url_for('data'))
+    return render_template('index.html')
+
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    if request.method == 'POST':
+        season = request.form.get("season")
+    else:
+        season = "Fall 2024"
+    
+    #TODO fetch both from DB
+    seasons = ["Fall 2024", "Spring 2023", "Fall 2023"] 
+    data = [
+        season + " Data...",
+        "ACT 1",
+        "ACT 2",
+        "ACT 3",
+        "ACT 4",
+    ]
+    return render_template('data.html', seasons=seasons, data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
