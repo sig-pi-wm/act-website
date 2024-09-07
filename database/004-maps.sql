fefe0cde07125a9a6fe3293aa0b8f04e32800b9f
Matthew Berthoud <mwberthoud@wm.edu>
2024-09-06 21:27:48 -0400

    maps and races tables

---
+++ database/004-maps.sql
@@ -0,0 +1,55 @@
+CREATE TABLE IF NOT EXISTS maps (
+    map_id INT AUTO_INCREMENT PRIMARY KEY,
+    map_name VARCHAR(255) NOT NULL,
+    cup VARCHAR(255) NOT NULL
+);
+
+INSERT INTO maps (map_name, cup)
+VALUES
+    -- Mushroom Cup
+    ('Luigi Circuit', 'Mushroom'),
+    ('Moo Moo Meadows', 'Mushroom'),
+    ('Mushroom Gorge', 'Mushroom'),
+    ('Toad''s Factory', 'Mushroom'),
+    
+    -- Flower Cup
+    ('Mario Circuit', 'Flower'),
+    ('Coconut Mall', 'Flower'),
+    ('DK Summit', 'Flower'),
+    ('Wario''s Gold Mine', 'Flower'),
+    
+    -- Star Cup
+    ('Daisy Circuit', 'Star'),
+    ('Koopa Cape', 'Star'),
+    ('Maple Treeway', 'Star'),
+    ('Grumble Volcano', 'Star'),
+    
+    -- Special Cup
+    ('Dry Dry Ruins', 'Special'),
+    ('Moonview Highway', 'Special'),
+    ('Bowser''s Castle', 'Special'),
+    ('Rainbow Road', 'Special'),
+    
+    -- Shell Cup
+    ('GCN Peach Beach', 'Shell'),
+    ('DS Yoshi Falls', 'Shell'),
+    ('SNES Ghost Valley 2', 'Shell'),
+    ('N64 Mario Raceway', 'Shell'),
+    
+    -- Banana Cup
+    ('N64 Sherbet Land', 'Banana'),
+    ('GBA Shy Guy Beach', 'Banana'),
+    ('DS Delfino Square', 'Banana'),
+    ('GCN Waluigi Stadium', 'Banana'),
+    
+    -- Leaf Cup
+    ('DS Desert Hills', 'Leaf'),
+    ('GBA Bowser Castle 3', 'Leaf'),
+    ('N64 DK''s Jungle Parkway', 'Leaf'),
+    ('GCN Mario Circuit', 'Leaf'),
+    
+    -- Lightning Cup
+    ('SNES Mario Circuit 3', 'Lightning'),
+    ('DS Peach Gardens', 'Lightning'),
+    ('GCN DK Mountain', 'Lightning'),
+    ('N64 Bowser''s Castle', 'Lightning');
---
+++ database/005-races.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS races (
+    race_id INT AUTO_INCREMENT PRIMARY KEY,
+    act_id INT,
+    map_id INT,
+    race_number INT NOT NULL, -- 1-16, generally
+
+    t1_player_uid INT NOT NULL,
+    t2_player_uid INT NOT NULL,
+    t3_player_uid INT,
+    t4_player_uid INT,
+
+    FOREIGN KEY (act_id) REFERENCES acts(act_id),
+    FOREIGN KEY (map_id) REFERENCES maps(map_id)
+);
