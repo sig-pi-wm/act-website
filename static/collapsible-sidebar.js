f6f9bcdf86da510748dd69356a4a2cb290cdc692
Matthew Berthoud <mwberthoud@wm.edu>
2024-10-21 00:40:15 -0400

    starting to add click action

---
+++ static/collapsible-sidebar.js
@@ -0,0 +1,6 @@
+const handleClick = () => {
+    console.log("click");
+};
+
+const hamburgerMenuButton = document.getElementById("hamburger-menu-button");
+hamburgerMenuButton.addEventListener('click', handleClick);
